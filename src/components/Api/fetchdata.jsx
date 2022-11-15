6bdcde1d845093919c54fca3b4404edb637678a3
Victor Nikolenko <victor1982nik@gmail.com>
2022-11-15 11:19:32 +0200

    monday

---
+++ src/components/Api/fetchdata.jsx
@@ -0,0 +1,9 @@
+/*
+//const API_KEY = 'https://api.themoviedb.org/3/movie/550?api_key=368aa8eefea213f2e59844c75600bcb3'
+
+https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>
+https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>&language=en-US
+https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en-US&page=1
+*/
